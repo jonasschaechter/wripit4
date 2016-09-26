6c756cf84dbe76d73a6cbd3f35a0cb857eb83e71
JMAC <info@schaechter.biz>
2016-09-26 15:45:32 +0200

    Aufnehmen und Abspielen

--- app/pages/main/main.ts
+++ app/pages/main/main.ts
@@ -3,17 +3,76 @@ import { NavController } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import { SettingsPage } from '../settings/settings';
 import { StatisticPage } from '../statistic/statistic';
-//import { MediaCapture } from 'ionic-native';
+import { MediaPlugin } from 'ionic-native';
 
 
 @Component({
   templateUrl: 'build/pages/main/main.html',
 })
 export class MainPage {
+media: MediaPlugin;
+
+  ionViewDidEnter() {
+    this.media = new MediaPlugin('../Library/NoCloud/recording.wav')
+  }
+
+
 
   constructor(private navCtrl: NavController, public alertCrtl: AlertController) {
     this.navCtrl = navCtrl;
+     
   }
+ 
+
+  //Recording functions including Error Messages
+  startRecording() {
+  try {
+    this.media.startRecord();
+  }
+  catch (e) {
+    this.showAlert('Could not start recording.');
+  }
+}
+
+stopRecording() {
+  try {
+    this.media.stopRecord();
+  }
+  catch (e) {
+    this.showAlert('Could not stop recording.');
+  }
+}
+
+startPlayback() {
+  try {
+    this.media.play();
+  }
+  catch (e) {
+    this.showAlert('Could not play recording.');
+  }
+}
+
+stopPlayback() {
+  try {
+    this.media.stop();
+  }
+  catch (e) {
+    this.showAlert('Could not stop playing recording.');
+  }
+}
+
+
+//show error message
+showAlert(message) {
+  let alert = this.alertCrtl.create({
+    title: 'Error',
+    subTitle: message,
+    buttons: ['OK']
+  });
+  alert.present();
+}
+
+//show popup
 doAlert() {
  let alert = this.alertCrtl.create({
         title: 'Statistics',
@@ -25,7 +84,10 @@ doAlert() {
   }
 //link to Settings
   goToSettings(){
-this.navCtrl.push(SettingsPage);
+this.navCtrl.push(SettingsPage, {
+direction: 'back',
+animation: false 
+});
 
   }
 
